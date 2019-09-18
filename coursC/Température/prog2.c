71deb2714d8336e7ee22a9c94ac9fd7f8b57828a
Thibault Pech <42326001+SRThibaultP@users.noreply.github.com>
2019-09-18 14:16:12 +0200

    Update prog2.c

--- coursC/Température/prog2.c
+++ coursC/Température/prog2.c
@@ -21,39 +21,45 @@ unsigned int read_a2d(unsigned char);
 void putch(char c);
 void init_serie(void);
 char getch(void);
-
-float T = 0, N=0;
+float T = 0, N=25;
 char c=0;
+char serialM;
 //void getString(char * a, unsigned int b );
 
 void main()
 {
 
-    // Appel des fonctions initialisation des entr�es sorties de la liaison s�rie et du CAN
+    // Appel des fonctions initialisation des entr?es sorties de la liaison s?rie et du CAN
 	init_ES();
     init_serie();
     init_a2d();
-
+printf("Appuyez sur T pour afficher la température ou appuyez sur Q pour redémarrer\r\n ");
      while(1)
   	{
-
-        N = read_a2d(0);  // Lecture de la valeur renvoy�e par le C.A.N
-        T = N/2.046; // T en fonction De N
-		printf("T = %.2f %cC\r\n", T,248);    // Envoi vers le Terminal de la valeur de la temp�rature
-	    __delay_ms(100);
+			
+			serialM = getch();
+			if(serialM == 't' || serialM == 'T'){
+				N = read_a2d(1);  // Lecture de la valeur renvoy?e par le C.A.N
+				T = N/2.046; // T en fonction De N
+				printf("T = %.2f °C\r\n", T);    // Envoi vers le Terminal de la valeur de la temp?rature
+				__delay_ms(100);
+			}
+			else if (serialM == 'q' || serialM == 'Q') {
+				break;
+			}
 
 	}
 }
 
 
 
-// Fonction initialisation des entr�es sorties
+// Fonction initialisation des entr?es sorties
 
 void init_ES(void)
 {
 	TRISD=0x00; // Configuratio PORTD en sortie
-    TRISA0=1;
-    ANS0=1;
+    TRISA1=1;
+    ANS1=1;
 	TRISC6=0;
 }
 
@@ -67,7 +73,7 @@ void init_a2d(void)
 	ADON=1;		// turn on the A2D conversion module
 }
 
-// Fonction lecture r�sultat de la conversion analogique num�rique
+// Fonction lecture r?sultat de la conversion analogique num?rique
 unsigned int read_a2d(unsigned char channel){
 	channel&=0x0F;	// truncate channel to 4 bits
 	ADCON0&=0xC3;	// clear current channel select
@@ -77,13 +83,13 @@ unsigned int read_a2d(unsigned char channel){
 	return(256*ADRESH + ADRESL);	// return 8 MSB of the result
 }
 
-// Initialisation de la liaison s�rie du PIC 16F887
+// Initialisation de la liaison s?rie du PIC 16F887
 void init_serie(void)
 {
     SPBRG = 25;       // 9600 bauds pour un quartz de 4 MHz
     TXSTA = 0b00100100; //transmit enable, async, high speed mode
     RCSTA = 0b10000000; //serial port enable
-    // Validation de l'�mission et de la r�ception de la liaison s�rie du PIC 16F887
+    // Validation de l'?mission et de la r?ception de la liaison s?rie du PIC 16F887
     TXEN=1;
     BRGH=1;
     SPEN=1;
@@ -92,24 +98,24 @@ void init_serie(void)
 
 }
 
-// Ecriture d'un caract�re
+// Ecriture d'un caract?re
 void putch(char data)
 {
     while(!TRMT);  // Attente buffer vide
-    TXREG = data;  // envoi caract�re
+    TXREG = data;  // envoi caract?re
 }
 
-// lecture d'un caract�re
+// lecture d'un caract?re
 char getch()
 {
-    while(!RCIF);  // Attente arriv�e caract�re
-    return RCREG;  // Renvoi du caract�re re�u
+    while(!RCIF);  // Attente arriv?e caract?re
+    return RCREG;  // Renvoi du caract?re re?u
 }
 
-// saisie d'une cha�ne de caract�res termin� par CR
+// saisie d'une cha?ne de caract?res termin? par CR
 void getString(char *input, unsigned int length)
 {
-    for(int i=0;i<length;i++)    // Lecture d'une cha�ne de cat�res de longueur length
+    for(int i=0;i<length;i++)    // Lecture d'une cha?ne de cat?res de longueur length
     {
         input[i] = getch();        //acquire each character until lenght chars are received
         if(input[i]==13)  break;   //or if newline is received
