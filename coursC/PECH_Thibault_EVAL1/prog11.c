7ce2d9ad66efcf9337aff0f19247f1b97fa06265
Thibault Pech <42326001+SRThibaultP@users.noreply.github.com>
2019-09-25 17:33:43 +0200

    EVAL1

---
+++ coursC/PECH_Thibault_EVAL1/prog11.c
@@ -0,0 +1,141 @@
+
+#include <xc.h>
+#include <stdio.h>
+// BEGIN CONFIG
+#pragma config FOSC = HS // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF // Watchdog Timer Enable bit (WDT enabled)
+#pragma config PWRTE = ON// Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF // Flash Program Memory Code Protection bit (Code protection off)
+//END CONFIG
+
+#define _XTAL_FREQ 4000000
+
+// prototypes des fonctions
+void init_a2d(void);
+void init_ES(void);
+unsigned int read_a2d(unsigned char);
+void putch(char c);
+void init_serie(void);
+char getch(void);
+float T = 0, temperature=0;
+char c=0;
+char serialM;
+//void getString(char * a, unsigned int b );
+
+void main()
+{
+
+    // Appel des fonctions initialisation des entr�es sorties de la liaison s�rie et du CAN
+	init_ES();
+    init_serie();
+    init_a2d();
+
+     while(1)
+  	{
+      temperature = read_a2d(1);
+      temperature = temperature/2.046;
+      if (temperature < 17) {
+        RC0 = 1;
+	 printf("LEDR/chauffage on\r\n");
+      }
+         if (temperature >= 20) {
+           RC0 = 1;
+   	 printf("LEDR/chauffage on\r\n");
+      }
+            if (temperature > 25) {
+              RC0 = 1;
+      	 printf("LEDR/chauffage on\r\n");
+      }
+
+      if (temperature <= 22) {
+        RC0 = 1;
+	 printf("LEDR/chauffage on\r\n");
+      }
+      printf("T = %.2f %cC\r\n", temperature,248);    // Envoi vers le Terminal de la valeur de la temp�rature
+//printf("%s\n",serialM );
+      __delay_ms(1000);
+	}
+}
+
+
+
+// Fonction initialisation des entr�es sorties
+
+void init_ES(void)
+{
+   PORTC=0;
+	TRISD=0x00; // Configuratio PORTD en sortie
+    TRISA1=1;
+    ANS1=1;
+   PORTC=0;
+   TRISC=0;
+	TRISC0=0;
+   TRISC1=0;
+   TRISC2=0;
+}
+
+
+
+// Fonction initialisation du CAN
+void init_a2d(void)
+{
+	ADCON0=0xC0;	// select Fosc/2
+	ADCON1=0x80;	// select rightjustify result. A/D port configuration 0
+	ADON=1;		// turn on the A2D conversion module
+}
+
+// Fonction lecture r�sultat de la conversion analogique num�rique
+unsigned int read_a2d(unsigned char channel){
+	channel&=0x0F;	// truncate channel to 4 bits
+	ADCON0&=0xC3;	// clear current channel select
+	ADCON0|=(channel<<2);	// apply the new channel select
+    GO=1;	// initiate conversion on the selected channel
+	while(GO)continue;
+	return(256*ADRESH + ADRESL);	// return 8 MSB of the result
+}
+
+// Initialisation de la liaison s�rie du PIC 16F887
+void init_serie(void)
+{
+    SPBRG = 25;       // 9600 bauds pour un quartz de 4 MHz
+    TXSTA = 0b00100100; //transmit enable, async, high speed mode
+    RCSTA = 0b10000000; //serial port enable
+    // Validation de l'�mission et de la r�ception de la liaison s�rie du PIC 16F887
+    TXEN=1;
+    BRGH=1;
+    SPEN=1;
+    CREN=1;
+
+
+}
+
+// Ecriture d'un caract�re
+void putch(char data)
+{
+    while(!TRMT);  // Attente buffer vide
+    TXREG = data;  // envoi caract�re
+}
+
+// lecture d'un caract�re
+char getch()
+{
+    while(!RCIF);  // Attente arriv�e caract�re
+    return RCREG;  // Renvoi du caract�re re�u
+}
+
+// saisie d'une cha�ne de caract�res termin� par CR
+void getString(char *input, unsigned int length)
+{
+    for(int i=0;i<length;i++)    // Lecture d'une cha�ne de cat�res de longueur length
+    {
+        input[i] = getch();        //acquire each character until lenght chars are received
+        if(input[i]==13)  break;   //or if newline is received
+
+    }
+
+    printf("Il est : %.10s\r",input);                        //print input string
+}
