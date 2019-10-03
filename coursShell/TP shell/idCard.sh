f14bc69437db36db30651062661d5be7f674e5b2
Thibault <42326001+SRThibaultP@users.noreply.github.com>
2019-10-03 11:28:47 +0200

    age marche pas

--- coursShell/TP shell/idCard.sh
+++ coursShell/TP shell/idCard.sh
@@ -9,14 +9,9 @@ read adresse
 echo "Insérez votre numéro de téléphone"
 read tel
 
-anneeActuel=`date +%Y`
-anneeID=`$birth | cut -d'/' -f3`
-age=$(echo "$anneeID - $anneeActuel" | bc)
-
 clear
 echo "Votre carte d'identité :"
 echo "Nom, Prénom :" $nomPrenom
 echo "Né(e) le :" $birth
-echo "Age :" $age
 echo "Adresse :" $adresse
 echo "Numéro de téléphone :" $tel
