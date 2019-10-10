3b412c23b01634e81db5de9e1efe3d00da8a5264
Thibault Pech <42326001+SRThibaultP@users.noreply.github.com>
2019-10-10 16:55:13 +0200

    Shell : Modif comm bc

--- coursShell/TP shell/addition.sh
+++ coursShell/TP shell/addition.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-resultat=$(echo "$1 + $2" | bc) #bc = commande de calcul
+resultat=$(echo "$1 + $2" | bc) #bc = commande de calcul, |(pipe) passe d'une chaine de caractère à un argument
 echo $resultat
