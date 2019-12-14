fb3f891028af0af8f4e36cc68727b89d720ebeed
Thibault PECH <42326001+SRThibaultP@users.noreply.github.com>
2019-12-14 19:46:26 +0100

    Python : Détection du choix, commentaire de code

--- coursPython/TP9/ConvertisseurTemperature.py
+++ coursPython/TP9/ConvertisseurTemperature.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import *                                                           # Ajout de la librairie tkinter
 
 def conversion(arg=None):
     valeur = entree.get()
@@ -19,9 +19,10 @@ def tempKelvin(cel):
 def tempFahrenheit(kel):
     return(kel*9/5-459.67)
 
-def selection(evt):
-    value = str((listTemp.get(ACTIVE)))
-    print(value)
+def selection(event):                                                           # Détection du choix de conversion avec event (ici le choix d'une conversion)
+    ligne = listTemp.curselection()                                             # Détection de la selection dans la liste (aka chiffre)
+    ligneTxt = listTemp.get(ligne)                                              # Récupération du texte (aka conversion dans la liste)
+    labelResultatconversion.config(text=ligneTxt)                               # Affichage du choix dans le résultat
 
 fen1 = Tk()
 fen1.title("Convertisseur Température °C/°F/°K")
@@ -34,20 +35,17 @@ entree = Entry(fen1,)
 entree.bind("<Return>", conversion)
 entree.grid(row=0, column=1, padx=5, pady=5)
 
-listeComplete = ["°C->°F", "°C->°K", "°K->°F", "°K->°C", "°F->°K"]
 listTemp = Listbox(fen1, height=5, bg="lightblue", fg="yellow")
-listTemp.bind('<<ListboxSelect>>', selection)
+for listeComplete in ["°C->°F", "°C->°K", "°K->°F", "°K->°C", "°F->°K"]:        # Liste des conversions
+    listTemp.insert(END, listeComplete)
 listTemp.grid(row=0, column=2, padx=5, pady=5)
-
+listTemp.bind("<<ListboxSelect>>", selection)                                   # Appelle la définition selection() lors d'un choix
 listTemp.select_set(0)
 
-labelResultatconversion = Label(fen1, width=30, height=4, bg="lightgreen", fg="blue")
+labelResultatconversion = Label(fen1, width=30, height=4, text="°C->°F", bg="lightgreen", fg="blue")
 labelResultatconversion.grid(row=1, column=1)
 
 boutonQuitter = Button(fen1, text="Quitter", bg="purple", command = fen1.destroy)
 boutonQuitter.grid(row=1, column=2, sticky=W, padx=50, pady=5)
 
-for i in listeComplete:
-    listTemp.insert(END,i)
-
-listTemp.mainloop()
+mainloop()
