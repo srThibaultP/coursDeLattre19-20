3f3659b870bce372fb54a5b76b463ff217f2e0f0
Thibault PECH <42326001+SRThibaultP@users.noreply.github.com>
2019-12-24 15:50:30 +0100

    Web : Passage en php

---
+++ coursWeb/website/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Index</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <h4>PECH Thibault BTSSN1</h4>
+  <h1>Index</h1>
+
+  <h2><a href="http://localhost" class="nomTP" title="Le 10 octobre 2019">TP1</a></h2>
+  <h3>Création du serveur Apache</h3>
+
+  <h2><a href="TP2" class="nomTP" title="Le 7 novembre 2019">TP2</a></h2>
+  <h3>Initiation au HTML</h3>
+  <ul>
+    <li><a href="TP2/exe1.html">Exe 1 : Emploi du temps</a></li>
+    <li><a href="TP2/exe2.html">Exe 2 : Curriculum vitæ</a></li>
+  </ul>
+
+  <h2><a href="TP3" class="nomTP" title="Le 14 novembre 2019">TP3</a></h2>
+  <h3>Initiation au CSS</h3>
+  <ul>
+    <li><a href="TP3/index.html">Page d'accueil</a></li>
+    <li><a href="TP3/cvcss.html">Ajout du CSS dans le CV</a></li>
+  </ul>
+</body>
+
+</html>
