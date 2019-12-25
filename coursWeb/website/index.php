d7c3403e3f82061daf022ad277de346a0c878fc6
Thibault PECH <42326001+SRThibaultP@users.noreply.github.com>
2019-12-26 00:34:06 +0100

    Web : Maj de l'index en cours

--- coursWeb/website/index.php
+++ coursWeb/website/index.php
@@ -8,32 +8,47 @@
 </head>
 
 <body>
-  <h4>PECH Thibault BTSSN1</h4>
-  <h1>Index</h1>
+  <header>
+    <h1>Historique des TP</h1>
+  </header>
 
-  <h2><a href="http://localhost" class="nomTP" title="Le 10 octobre 2019">TP1</a></h2>
-  <h3>Création du serveur Apache</h3>
+  <nav>
+    <h3>Liste des TP</h3>
+    <a href="#TP1"><p>TP1</p></a>
+    <a href="#TP2"><p>TP2</p></a>
+    <a href="#TP3"><p>TP3</p></a>
+    <a href="#TP4"><p>TP4</p></a>
+  </nav>
 
-  <h2><a href="TP2" class="nomTP" title="Le 7 novembre 2019">TP2</a></h2>
-  <h3>Initiation au HTML</h3>
-  <ul>
-    <li><a href="TP2/exe1.html">Exe 1 : Emploi du temps</a></li>
-    <li><a href="TP2/exe2.html">Exe 2 : Curriculum vitæ</a></li>
-  </ul>
+  <section>
+    <h2><a href="http://localhost" id="TP1" class="nomTP" title="Le 10 octobre 2019">TP1</a></h2>
+    <h3>Création du serveur Apache</h3>
 
-  <h2><a href="TP3" class="nomTP" title="Le 14 novembre 2019">TP3</a></h2>
-  <h3>Initiation au CSS</h3>
-  <ul>
-    <li><a href="TP3/index.html">Page d'accueil</a></li>
-    <li><a href="TP3/cvcss.html">Ajout du CSS dans le CV</a></li>
-  </ul>
+    <h2><a href="TP2" id="TP2" class="nomTP" title="Le 7 novembre 2019">TP2</a></h2>
+    <h3>Initiation au HTML</h3>
+    <ul>
+      <li><a href="TP2/exe1.html">Exe 1 : Emploi du temps</a></li>
+      <li><a href="TP2/exe2.html">Exe 2 : Curriculum vitæ</a></li>
+    </ul>
 
-  <h2><a href="TP4" class="nomTP" title="Le 19 décembre 2019">TP4</a></h2>
-  <h3>Initiation au PHP</h3>
-  <ul>
-    <li><a href="TP4/rappel.html">Rappels HTML</a></li>
-    <li><a href="TP4/index.php">Page PHP</a></li>
-  </ul>
+    <h2><a href="TP3" id="TP3" class="nomTP" title="Le 14 novembre 2019">TP3</a></h2>
+    <h3>Initiation au CSS</h3>
+    <ul>
+      <li><a href="TP3/index.html">Page d'accueil</a></li>
+      <li><a href="TP3/cvcss.html">Ajout du CSS dans le CV</a></li>
+    </ul>
+
+    <h2><a href="TP4" id="TP4" class="nomTP" title="Le 19 décembre 2019">TP4</a></h2>
+    <h3>Initiation au PHP</h3>
+    <ul>
+      <li><a href="TP4/rappel.html">Rappels HTML</a></li>
+      <li><a href="TP4/index.php">Page PHP</a></li>
+      <li><a href="TP4/presentation.php">Présentation</a></li>
+    </ul>
+  </section>
+  <footer>
+    <p>PECH Thibault BTSSN1</p>
+  </footer>
 </body>
 
 </html>
