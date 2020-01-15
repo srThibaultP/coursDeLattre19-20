7b774045e90e9e4da3bfd26d6c2720c87af8d39b
Thibault PECH <42326001+SRThibaultP@users.noreply.github.com>
2020-01-15 10:44:28 +0100

    C++ : Exo 2 fix "<" uniquement

--- coursC++/branchementsConditionnels/exo2.cpp
+++ coursC++/branchementsConditionnels/exo2.cpp
@@ -7,9 +7,9 @@ int main() {
   cout << "Entrer un reel :" << endl;
   cin >> a;
 
-  bool b((a>2 || a==2) && a<3);
-  bool c(a>0 && (a<1 || a==1));
-  bool d((a>-10 || a==-10) && (a<-2 || a==-2));
+  bool b((2<a || a==2) && a<3);
+  bool c(0<a && (a<1 || a==1));
+  bool d((-10<a || a==-10) && (a<-2 || a==-2));
   bool e(b || c || d);
 
   if(e) {
