47cc54d363cce116ee2f9f236a2babe865632b9d
Thibault PECH <42326001+SRThibaultP@users.noreply.github.com>
2020-01-23 17:15:35 +0100

    PHP : TP6 en cours

---
+++ coursWeb/website/TP6/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP-3 les formulaires</title>
+  </head>
+  <body>
+    <a href="../index.html"><img src="/img/LogoSN.png" width="200" height="100" align="right"/></a>
+    <h1 align="middle">TP5 : PHP-3 les formulaires</h1>
+    <h2>PECH Thibault</h2>
+    <p><?php
+          $d = date("d/m/y ");
+          $h = date("H:i");
+          print("Le $d à $h");
+        ?></p>
+
+    <form action="traitementForm1.php" method="post">Nom
+      <input type="text" name="nom">
+      <button type="submit">Valider</button>
+    </form>
+
+    <form action="traitement.php" method="post">Adresse IP
+      <input type="number" name="ip1" maxlength="3" min="1" max="255">
+      <input type="number" name="ip2" maxlength="3" min="1" max="255">
+      <input type="number" name="ip3" maxlength="3" min="0" max="255">
+      <input type="number" name="ip4" maxlength="3" min="1" max="254">
+      <button type="submit">Valider</button>
+    </form>
+
+    <form action="traitement.php" method="post">Masque de sous réseau
+      <input type="number" name="ma1" maxlength="3" min="1" max="255">
+      <input type="number" name="ma2" maxlength="3" min="1" max="255">
+      <input type="number" name="ma3" maxlength="3" min="0" max="255">
+      <input type="number" name="ma4" maxlength="3" min="1" max="255">
+      <button type="submit">Valider</button>
+    </form>
+  </body>
+</html>
--- coursWeb/website/TP6/traitement.php
+++ coursWeb/website/TP6/traitement.php
@@ -1,4 +1,33 @@
 <?php
-$nom = $_POST['nom'];
-echo "Bonjour $nom !";
+$ip1 = $_POST['ip1'];
+$ip2 = $_POST['ip2'];
+$ip3 = $_POST['ip3'];
+$ip4 = $_POST['ip4'];
+
+$ma1 = $_POST['ma1'];
+$ma2 = $_POST['ma2'];
+$ma3 = $_POST['ma3'];
+$ma4 = $_POST['ma4'];
  ?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP-3 les formulaires</title>
+  </head>
+  <body>
+    <a href="./indexhtml"><img src="/img/LogoSN.png" width="200" height="100" align="right"/></a>
+    <h1 align="middle">TP5 : PHP-3 les formulaires</h1>
+    <h2>PECH Thibault</h2>
+    <p><?php
+          $d = date("d/m/y ");
+          $h = date("H:i");
+          print("Le $d à $h");
+        ?>
+    </p>
+
+    <p><?php echo "Votre adresse IP est $ip1.$ip2.$ip3.$ip4";?></p>
+    <p><?php echo "Votre masque de sous réseau est $ma1.$ma2.$ma3.$ma4";?></p>
+  </body>
+</html>
