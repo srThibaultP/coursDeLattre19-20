8830307ac2c4964d48a248ac40741ef0318cd8c8
Thibault PECH <42326001+SRThibaultP@users.noreply.github.com>
2020-01-23 11:40:41 +0100

    PHP : Ex1 fini

--- coursWeb/website/TP5/affiche.php
+++ coursWeb/website/TP5/affiche.php
@@ -1,15 +1,17 @@
 <?php
 $an = $_GET['an'];
-//echo gettype($an), "\n";
-//echo "$an", "\n";
-if(is_numeric($an)) {
-  echo "C'est l'année $an";
-}
-else {
-  echo "$an n'est pas une année";
-}
-/*
 if(isset($an)) {
-    echo "Année : $an";
-}*/
+  if(is_numeric($an)) {
+    $an = (int)$an;
+    if(strlen($an)>4) {
+      echo "$an n'est pas une année";
+    }
+    else {
+      echo "C'est l'année $an";  
+    }
+  }
+  else {
+    echo "$an n'est pas une année";
+  }
+}
  ?>
