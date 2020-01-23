6e47afa7001bafca66da71600e133de0fcd87a87
Thibault PECH <42326001+SRThibaultP@users.noreply.github.com>
2020-01-23 12:02:20 +0100

    PHP : is_numeric

--- coursWeb/website/TP5/calcul.php
+++ coursWeb/website/TP5/calcul.php
@@ -3,32 +3,39 @@ $valeur1 = $_GET['valeur1'];
 $valeur2 = $_GET['valeur2'];
 $operation = $_GET['operation'];
 
-if(isset($valeur1, $valeur2, $operation)) {
+//echo gettype($valeur1);
+//echo gettype($valeur2);
+
+if(isset($valeur1, $valeur2, $operation) && is_numeric($valeur1) && is_numeric($valeur2)) {
   //echo "$valeur1 $operation $valeur2";
   switch ($operation) {
     case 'addition':
     $resultat = $valeur1 + $valeur2;
     echo "$valeur1 + $valeur2 = $resultat";
       break;
+
     case 'soustraction':
     $resultat = $valeur1 - $valeur2;
     echo "$valeur1 - $valeur2 = $resultat";
       break;
+
     case 'multiplication':
     $resultat = $valeur1 * $valeur2;
     echo "$valeur1 * $valeur2 = $resultat";
       break;
+
     case 'division':
     $resultat = $valeur1 / $valeur2;
     echo "$valeur1 / $valeur2 = $resultat";
       break;
 
     default:
-      echo "Opérations disponibles : addition, soustraction, multiplication et division";
+      echo "Opérateurs disponibles : addition, soustraction, multiplication et division";
       break;
   }
 }
 else {
-  echo "Insérer des valeurs (valeur1, valeur2, operation)";
+  echo "Insérer des valeurs (valeur1, valeur2, operation)<br/>";
+  echo "Opérateurs disponibles : addition, soustraction, multiplication et division";
 }
  ?>
