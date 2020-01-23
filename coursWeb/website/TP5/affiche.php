6b04222a32530061e45ce70c35cf8d260831b381
Thibault PECH <42326001+SRThibaultP@users.noreply.github.com>
2020-01-23 11:26:57 +0100

    PHP : is_numeric

--- coursWeb/website/TP5/affiche.php
+++ coursWeb/website/TP5/affiche.php
@@ -1,6 +1,15 @@
 <?php
 $an = $_GET['an'];
-if(isset($an)) {
-  echo "Année : $an";
+//echo gettype($an), "\n";
+//echo "$an", "\n";
+if(is_numeric($an)) {
+  echo "C'est l'année $an";
 }
+else {
+  echo "$an n'est pas une année";
+}
+/*
+if(isset($an)) {
+    echo "Année : $an";
+}*/
  ?>
