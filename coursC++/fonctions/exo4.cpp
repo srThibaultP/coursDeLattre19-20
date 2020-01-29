0b4a56a5cde64664abbf0b1376d3b80708eb21b6
Thibault PECH <42326001+SRThibaultP@users.noreply.github.com>
2020-01-29 10:55:15 +0100

    C++ : Les fonctions exo 4 int -> double

--- coursC++/fonctions/exo4.cpp
+++ coursC++/fonctions/exo4.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int resultat(double val1, double val2);
+double resultat(double val1, double val2);
 
 int main() {
-  double val1, val2;
+  double val1, val2, total;
   cout << "Choisir deux valeurs : " << endl;
   cin >> val1 >> val2;
-  cout << resultat(val1, val2) << " est le total des operations" << endl;
+  total = resultat(val1, val2);
+  cout << total << " est le total des operations." << endl;
 }
 
-int resultat(double val1, double val2) {
+double resultat(double val1, double val2) {
   double resultat, resultat1;
   resultat = val1 + val2;
   cout << "Le resultat de l'addition est " << resultat << endl;
   resultat1 = val1 - val2;
-  cout << "Le resultat de la soustraction est " << resultat << endl;
+  cout << "Le resultat de la soustraction est " << resultat1 << endl;
   //cout << "resultat=" << resultat << "**resultat1=" << resultat1 <<endl;
 
   resultat = resultat + resultat1;
   //cout << "resultat=" << resultat << "**resultat1=" << resultat1 <<endl;
 
   resultat1 = val1 * val2;
-  cout << "Le resultat de la multiplication est " << resultat << endl;
+  cout << "Le resultat de la multiplication est " << resultat1 << endl;
   //cout << "resultat=" << resultat << "**resultat1=" << resultat1 <<endl;
 
   resultat = resultat + resultat1;
   //cout << "resultat=" << resultat << "**resultat1=" << resultat1 <<endl;
 
   resultat1 = val1 / val2;
-  cout << "Le resultat de la division est " << resultat << endl;
+  cout << "Le resultat de la division est " << resultat1 << endl;
   //cout << "resultat=" << resultat << "**resultat1=" << resultat1 <<endl;
-  resultat = resultat + resultat1;
-  return resultat;
+  return resultat + resultat1;
 }
