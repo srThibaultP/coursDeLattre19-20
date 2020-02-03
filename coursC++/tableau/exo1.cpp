f9fbe04254a683ac4ab4ea51f496b0df019391cb
Thibault PECH <42326001+SRThibaultP@users.noreply.github.com>
2020-02-03 20:45:42 +0100

    C++ : Exe tableau fini

--- coursC++/tableau/exo1.cpp
+++ coursC++/tableau/exo1.cpp
@@ -2,20 +2,32 @@
 #include <vector>
 using namespace std;
 
+void tableau();
+
 int main() {
-  vector < int > entier(3);
   cout << "Saisie de 3 entiers" << endl;
-  for (size_t i = 1; i < entier.size(); ++i) {
+  tableau();
+}
+
+void tableau() {
+  int j(1);
+  vector < int > entier(3);
+  for (size_t i = 1; i < entier.size() + j; ++i) {
     cout << "Entrez entier numero " << i << " : ";
     cin >> entier[i];
     if(entier[i] == 0) {
+      //cout << "Supression du tableau" << endl;
       entier.clear();
+      tableau();
     }
     else if(entier[i] < 0) {
+      //cout << "Supression de la derniere valeur" << endl;
+      i=i-2;
+      j=j+1;
       entier.pop_back();
     }
   }
-  for (size_t i = 0; i < entier.size(); i++) {
-    cout<<i+1<<entier[i];
+  for (size_t i = 1; i < entier.size() + j; i++) {
+    cout<<entier[i]<<" ";
   }
 }
